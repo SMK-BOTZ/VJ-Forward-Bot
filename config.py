61f18a67cf1874cfad2b1b772d45713aa3bca9c3
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2025-04-25 15:14:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 from os import environ 
 
@@ -13,9 +10,6 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "cluster0")
     BOT_OWNER = int(environ.get("BOT_OWNER", "6879821587"))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 class temp(object): 
     lock = {}
@@ -23,7 +17,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
