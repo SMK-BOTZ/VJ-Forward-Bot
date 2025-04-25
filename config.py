5598afd8a2c85cdd2ad07507a20ee31d43b65d49
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2025-04-25 14:40:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,13 +5,13 @@
 from os import environ 
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = int(environ.get("API_ID", "28055351"))
+    API_HASH = environ.get("API_HASH", "4acf3a28b9745eb39b25a91478f9504b")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7594003962:AAGzId8q2ioq1DfvxVOTq38NWhZXqGw2nsc") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://misssiyaofficial:zzx4322CU68rU8gw@cluster0.frhwbd1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "cluster0")
+    BOT_OWNER = int(environ.get("BOT_OWNER", "6879821587"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
