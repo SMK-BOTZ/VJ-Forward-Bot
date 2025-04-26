f4c40e030c8947c25eedafe7deaaf9818423058d
SMK BOTZ <163844722+SMK-BOTZ@users.noreply.github.com>
2025-04-26 11:18:29 +0530

    Update regix.py

--- plugins/regix.py
+++ plugins/regix.py
@@ -239,7 +239,7 @@ async def edit(user, msg, title, status, sts):
    estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
    estimated_total_time = estimated_total_time if estimated_total_time != '' else '0 s'
    if status in ["cancelled", "completed"]:
-      button.append([InlineKeyboardButton('• ᴄᴏᴍᴘʟᴇᴛᴇᴅ ​•', url=f'https://t.me/{Config.UPDATE_CHANNEL}'])
+      button.append([InlineKeyboardButton('• ᴄᴏᴍᴘʟᴇᴛᴇᴅ ​•', url=f'https://t.me/{Config.UPDATE_CHANNEL}')])
    else:
       button.append([InlineKeyboardButton('• ᴄᴀɴᴄᴇʟ', 'terminate_frwd')])
    await msg_edit(msg, text, InlineKeyboardMarkup(button))
